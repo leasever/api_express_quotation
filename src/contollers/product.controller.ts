1cd72179122467ccd35a8b76a8a1c2bd051512ca
leasever <leandrovegaabraham@gmail.com>
2023-09-27 00:20:44 -0500

    crud product

--- src/contollers/product.controller.ts
+++ src/contollers/product.controller.ts
@@ -1,43 +1,58 @@
 import { Request, Response } from "express";
 import { handleHttp } from "../utils/error.handle";
-import { getItems, insertItem } from "../services/product.service";
+import {
+  getItem,
+  getItems,
+  insertItem,
+  updateItem,
+  deleteItem,
+} from "../services/product.service";
 
 const getProducts = async (_req: Request, res: Response) => {
   try {
-    const responseItems = await getItems();
-    res.send(responseItems);
+    const response = await getItems();
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_GET_PRODUCTS", e);
   }
 };
 
-const getProduct = (req: Request, res: Response) => {
+const getProduct = async ({ params }: Request, res: Response) => {
   try {
+    const { id } = params;
+    const response = await getItem(id);
+    res.send(response);
   } catch (e) {
-    handleHttp(res, "ERROR_GET_PRODUCT");
+    handleHttp(res, "ERROR_GET_PRODUCT", e);
   }
 };
 
 const postProduct = async ({ body }: Request, res: Response) => {
   try {
-    const responseItem = await insertItem(body);
-    res.send(responseItem);
+    const response = await insertItem(body);
+    res.send(response);
   } catch (e) {
     handleHttp(res, "ERROR_POST_PRODUCT", e);
   }
 };
 
-const updateProduct = (req: Request, res: Response) => {
+const updateProduct = async ({ params, body }: Request, res: Response) => {
   try {
+    const { id } = params;
+    const response = await updateItem(id, body);
+    res.send(response);
   } catch (e) {
-    handleHttp(res, "ERROR_UPDATE_PRODUCT");
+    handleHttp(res, "ERROR_UPDATE_PRODUCT", e);
   }
 };
 
-const deleteProduct = (req: Request, res: Response) => {
+const deleteProduct = async ({ params }: Request, res: Response) => {
   try {
+    const { id } = params;
+    const response = await deleteItem(id);
+    res.send(response);
   } catch (e) {
-    handleHttp(res, "ERROR_DELETE_PRODUCT");
+    handleHttp(res, "ERROR_DELETE_PRODUCT", e);
   }
 };
 
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,13 +2,30 @@ import { Product } from "../interfaces/product.interface";
 import ProductModel from "../models/product.model";
 
 const insertItem = async (product: Product) => {
-  const responseInsert = await ProductModel.create(product);
-  return responseInsert;
+  const response = await ProductModel.create(product);
+  return response;
 };
 
 const getItems = async () => {
-  const responseItems = await ProductModel.find({});
-  return responseItems;
+  const response = await ProductModel.find({});
+  return response;
 };
 
-export { insertItem, getItems };
+const getItem = async (id: string) => {
+  const response = await ProductModel.findOne({ _id: id });
+  return response;
+};
+
+const updateItem = async (id: string, data: Product) => {
+  const response = await ProductModel.findOneAndUpdate({ _id: id }, data, {
+    new: true,
+  });
+  return response;
+};
+
+const deleteItem = async (id: string) => {
+  const response = await ProductModel.deleteOne({ _id: id });
+  return response;
+};
+
+export { insertItem, getItems, getItem, updateItem, deleteItem };
